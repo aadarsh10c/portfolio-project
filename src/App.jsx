b97b1d5c14765c8c19870fd8e67f3ae4abe39e75
Adarsh Kumar <adarsh010c@gmail.com>
2023-04-11 00:21:27 +0530

    Added NAvbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World !!
-    </h1>
+    <div>
+      <Navbar />
+    </div>
   )
 }
 
