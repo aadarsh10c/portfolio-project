57ec86d888f5bb5ccd6562afda0a55aaa50f5c98
Adarsh Kumar <adarsh010c@gmail.com>
2023-04-14 03:49:18 +0530

    Added portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
+import Portfolio from './components/Portfolio'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Portfolio />
       <SocialLinks />
     </div>
   )
